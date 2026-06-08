174fa3e5327e6f0fbf3e01a5d9f879f91e503c0c
Bhumika Jain <jainbhumika666@gmail.com>
2026-06-08 23:11:16 +0530

    frontend update

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-    baseURL: 'http://localhost:3000/floodguard',
+    // baseURL: 'http://localhost:3000/floodguard',
+    baseURL: 'https://flood-guard-6spy.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
